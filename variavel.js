8dd6af62f0d670113871aa874775bb1746a12b26
Emanoeli Franca <xemanoelifranca@gmail.com>
2024-10-05 13:40:46 -0300

    feat (script): variables and their types added

---
+++ variavel.js
@@ -0,0 +1,9 @@
+//saída
+console.log("Digite o nome do seu jogador");
+
+//declaração de variável
+let nickname = "Manuzinha mestra dos pokemons"
+
+//concatenação de uma mensagem fixa + uma variável
+console.log("Bem vindo" + nickname);
+console.log(nickname + "entrou no servidor");
