41a9a4a87c714b5bc40390ea7f1145f011ac954a
Emanoeli Franca <xemanoelifranca@gmail.com>
2024-10-05 14:02:04 -0300

    feat (script): variable types practices - added

---
+++ pratica1.js
@@ -0,0 +1,19 @@
+let potCafe = "Café pilão"
+let poteAcucar = "Açucar cristal"
+let poteBiscoito = "Biscoito Maizena"
+const messagemDaVovo = "Na cozinha da vovó hoje tem:"
+
+console.log(messagemDaVovo +
+    potCafe + "-" + 
+    poteAcucar + "-" + 
+    poteBiscoito
+);
+
+potCafe = "Café 3 corações"
+
+console.log(messagemDaVovo +
+    potCafe + "-" + 
+    poteAcucar + "-" + 
+    poteBiscoito
+);
+
---
+++ pratica2.js
@@ -0,0 +1,11 @@
+//nome, idade, numero de telefone, cpf, endereço, recebe pensão
+//string = armazenar texto
+//number = armazenar numeros
+//boolean = armazenar valores lógicos
+
+let nomePessoa = "Josicreide do Pythinho"
+let idade = 115
+let numeroTelefone = "11-98471388"
+let cpf = "312.122.312-45"
+let endereco = "Rua dos limoeiros, numero 75, bairro"
+let recebePensao = true
