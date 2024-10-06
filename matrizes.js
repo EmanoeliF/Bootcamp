f83f934546b5b7016d19ee67bb318abaa2b9aa54
Emanoeli Franca <xemanoelifranca@gmail.com>
2024-10-06 07:42:02 -0300

    feat (script):matrices and vectors practice - added

---
+++ matrizes.js
@@ -0,0 +1,8 @@
+let nomesPokemon = ["Pikachu", "Chamander", "Bulbassaur"]
+let timePokemon = [
+    ["Pikachu", M, 1],
+    ["Chamander", F, 3]
+]
+
+
+console.log("O pokemon" + timePokemon [1][0] + "é do sexo" + timePokemon[1][1] + "e está no nível" + timePokemon[1][2] )
