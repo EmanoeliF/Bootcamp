583448ceb8f54c6a00e89a0bdb7fa53c7a4f0d76
Emanoeli Franca <xemanoelifranca@gmail.com>
2024-10-07 17:13:50 -0300

    feat(script): file structuring - added

---
+++ introducao-logica-programacao/variaveis/js/script.js
@@ -0,0 +1,32 @@
+// Declarando variáveis
+
+//saída
+console.log("Digite o nome do seu jogador");
+
+let nickname = "Manuzinha mestra dos pokemons"
+
+//concatenação de uma mensagem fixa + uma variável
+console.log("Bem vindo" + nickname);
+console.log(nickname + "entrou no servidor");
+
+
+
+
+//Costantes e variáveis
+const notificacao = "Pokemon Go diz: "
+
+//saida
+console.log(notificacao + "tem um novo pokemon na região");
+console.log(notificacao + "você foi derrotado por um líder");
+
+
+
+
+//Tipos primitivos de variáveis
+
+//pokemon
+let nomePokemon = "Pikachu"
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+let pokemonSexo = "M"
+let selecionavel = true
