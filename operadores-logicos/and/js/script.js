3f65be8fc3a3d92e1d9ba2b6df533773fb07e172
Emanoeli Franca <xemanoelifranca@gmail.com>
2024-10-08 11:07:53 -0300

    feat(script): logical operators - added

---
+++ operadores-logicos/and/js/script.js
@@ -0,0 +1,15 @@
+// AND ( && )
+
+let idade = 18
+let vistoVerificado = false
+let resultadoV =  (idade >= 18) && (vistoVerificado === true) 
+console.log(resultadoV)
+
+
+
+// AND ( && ) - 100 moedas coletadas E 1 item estrela
+
+let moedasColetadas = 99
+let item = "estrela"
+let resultado = (moedasColetadas >= 100) && (item === "estrela")
+console.log(resultado)
---
+++ operadores-logicos/not/js/script.js
@@ -0,0 +1,15 @@
+// NOT  ( !) - nega uma afirmação
+
+let tempoN = "chuva"
+let resultadoT = tempo === "chuva"
+
+console.log(!!resultado)
+
+
+
+// NOT  ( !) - nega uma afirmação
+let tempo = "chuva"
+let horario = 8
+let resultado = !((tempo !== "chuva") && (horario > 6))
+
+console.log(resultado)
