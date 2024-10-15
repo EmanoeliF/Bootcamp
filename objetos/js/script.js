53bb8b1bd5fdc580ee3eea36afe124780d06226b
Emanoeli Franca <xemanoelifranca@gmail.com>
2024-10-15 10:55:05 -0300

    feat(object):working with classes and objects - added

---
+++ objetos/js/script.js
@@ -0,0 +1,15 @@
+// Criando objeto
+
+let convidado = {
+    nome: "Pedrinho",
+    idade: 10,
+    presente: "quebra-cabeça"
+  };
+  console.log(convidado.nome + " tem " + convidado.idade + " anos e trouxe um " + convidado.presente + " de presente!");
+  
+  convidado.localizacao = "Sala de Estar";
+
+  console.log(convidado.nome + " está na " + convidado.localizacao + ".");
+
+  
+    
